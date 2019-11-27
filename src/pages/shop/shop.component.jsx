600cc7bfd06ed9eeb56f279711732d01d28d0179
luther <luther1096@gmail.com>
2019-11-27 19:46:41 +0530

    added directory and collection states in to redux state, built routing and display part of Collections page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,55 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
+// import SHOP_DATA from "./shop.data";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionOverveiw from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
+// import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
+// import { selectCollections } from "../../redux/shop/shop.selectors";
+// import { connect } from "react-redux";
+// import { createStructuredSelector } from "reselect";
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-name">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+import { Route } from "react-router-dom";
+import CollectionPage from "../collection/collection.component";
+
+// class ShopPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       collections: SHOP_DATA
+//     };
+//   }
+
+//   render() {
+//     const { collections } = this.state;
+//     return (
+//       <div className="shop-name">
+//         {collections.map(({ id, ...otherCollectionProps }) => (
+//           <CollectionPreview key={id} {...otherCollectionProps} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+const ShopPage = ({ match }) => (
+  // <div className="shop-name">
+  //   {collections.map(({ id, ...otherCollectionProps }) => (
+  //     <CollectionPreview key={id} {...otherCollectionProps} />
+  //   ))}
+  // </div>
+
+  <div className="shop-paage">
+    <Route exact path={`${match.path}`} component={CollectionOverveiw} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
+
+// const mapStateToProps = createStructuredSelector({
+//   collections: selectCollections
+// });
+
+// export default connect(mapStateToProps)(ShopPage);
 
 export default ShopPage;
